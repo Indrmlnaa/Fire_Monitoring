ab77ca90a203c1aba3e3ab89d52c281a62a45f92
Indra Maulana <Indramaulana1805@gmail.com>
2026-08-12 10:59:44 +0900

    Fix OKI boundary path

--- scripts/run_pipeline_v2.py
+++ scripts/run_pipeline_v2.py
@@ -17,8 +17,7 @@ MAP_KEY = os.environ["FIRMS_MAP_KEY"]
 
 AREA_NAME = "OKI"
 
-BOUNDARY_FILE = (
-    "boundaries\OKI\OKI.shp"
+BOUNDARY_FILE = "boundaries/OKI/OKI.shp"
 )
 
 from datetime import date, timedelta
